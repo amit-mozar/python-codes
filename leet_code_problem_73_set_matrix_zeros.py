9d4dce7f49007e03635f9a01c01edbbe1c416d94
amit-mozar <amit1406mozar@gmail.com>
2026-07-13 23:31:58 +0530

    this is also brute force, but in between of optimal and full brute force

--- leet_code_problem_73_set_matrix_zeros.py
+++ leet_code_problem_73_set_matrix_zeros.py
@@ -20,3 +20,21 @@ for i in range(rows):
         if matrix[i][j] == float('inf'):
             matrix[i][j] = 0
 print(matrix)
+
+#method 2 -> this is also brute force, but in between of optimal and full brute force
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+rows =len(matrix)
+cols = len(matrix[0])
+zero_matrix = []
+for i in range(rows):
+    for j in range(cols):
+        if matrix[i][j] == 0:
+            zero_matrix.append([i, j])
+r = len(zero_matrix)
+c = len(zero_matrix[0])
+for row, col in zero_matrix:
+    for k in range(cols):
+        matrix[row][k] = 0
+    for k in range(rows):
+        matrix[k][col] = 0
+print(matrix)
